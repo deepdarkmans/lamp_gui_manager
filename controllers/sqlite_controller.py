b0163571e31114572dd6303292d6be6cb8457cf4
deepdarkman <scheglovmx@gmail.com>
2020-02-11 21:03:35 +0500

    full functional design, actions note done

--- controllers/sqlite_controller.py
+++ controllers/sqlite_controller.py
@@ -5,27 +5,44 @@ import sys
 
 
 def initialDatabase():
-    if os.path.exists('databases/projects') is False:
+    if os.path.exists('databases/projects.db') is False:
         conn = sqlite3.connect('databases/projects')
         cursor = conn.cursor()
         cursor.execute("""CREATE TABLE projects
-                          (id INTEGER PRIMARY KEY, name varchar(255), host varchar(255), folder varchar(255),
-                          www_folder varchar(255), port varchar(255), status INTEGER)
+                          (id INTEGER PRIMARY KEY, name TEXT, host TEXT, folder TEXT,
+                          www_folder TEXT, port TEXT, status INTEGER)
                        """)
+        conn.close()
+        return
     else:
         return True
 
+
 def getDatabase():
-    if os.path.exists('databases/projects') is True:
-        conn = sqlite3.connect("databases/projects")
+    if os.path.exists('databases/projects.db') is True:
+        conn = sqlite3.connect('databases/projects.db')
         cursor = conn.cursor()
         data = cursor.execute("""SELECT * FROM projects""")
-        return data
+        rows = data.fetchall()
+        conn.close()
+        return rows
     else:
         initialDatabase()
 
-def addData():
-    conn = sqlite3.connect('databases/projects')
+
+def addData(name, host_name, folder, www_folder, a2_conf, port, status):
+    conn = sqlite3.connect('databases/projects.db')
     cursor = conn.cursor()
-    add = cursor.execute("""INSERT INTO projects(id,name,host_name,folder,www_folder,a2_conf,port,status) VALUES (2,'test1','rest1.local','testtses','fold','conf',80,1);""")
-    print(add)
+    cursor.execute("""INSERT INTO projects (name,host_name,folder,www_folder,a2_conf,port,status) VALUES (?,?,?,?,?,
+    ?,?);""", [name, host_name, folder, www_folder, a2_conf, port, status])
+    conn.commit()
+    conn.close()
+    return
+
+def getProject(name):
+    conn = sqlite3.connect('databases/projects.db')
+    cursor = conn.cursor()
+    data = cursor.execute("""SELECT * FROM projects WHERE name = ?""", [name])
+    rows = data.fetchall()
+    conn.close()
+    return rows
--- main.py
+++ main.py
@@ -1,23 +1,159 @@
 #!/usr/bin/env python3
-# version 0.0.1
+# version 0.0.1 dev
 import sys
 from PyQt5 import QtWidgets
 from gui import design
 import os
 import controllers.sqlite_controller
 import subprocess
-import sqlite3
 
-class appGUI(QtWidgets.QMainWindow, design.Ui_MainWindow):
+
+def get_projects_data():
+    return controllers.sqlite_controller.getDatabase()
+
+
+def add_data(name, host_name, folder, www_folder, a2_conf, port, status):
+    controllers.sqlite_controller.addData(name, host_name, folder, www_folder, a2_conf, port, status)
+
+
+def get_server_info(server):
+    result = subprocess.run(['systemctl', 'status', server, '|', 'grep', 'Active'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    lines = result.stdout
+    result = lines.decode("utf-8")
+    a = result.find('Active: ')
+    status1 = result[a:]
+    b = status1.find('(')
+    status = status1[:b]
+    c = status.find(': ')
+    status = status[c:]
+    status = status[1:]
+    return status
+
+
+def choose_project(it, col):
+    return controllers.sqlite_controller.getProject(it.text(col))
+
+
+def read_hosts():
+    file = open('/etc/hosts', 'r')
+    hosts = file.readlines()
+    return hosts
+
+
+def get_host_ip(host_name):
+    rows = read_hosts()
+    for row in rows:
+        if host_name in row:
+            host_name_find = row.find(host_name)
+            ip = row[:host_name_find]
+            return ip
+
+
+def get_git_url(folder):
+    try:
+        file = open(folder + '/.git/config', 'r')
+        config = file.readlines()
+    except:
+        return 'no git repository'
+    for line in config:
+        if 'url' in line:
+            a = line.find('url = ')
+            url = line[a:]
+            url = url[6:]
+            return url
+
+
+class AppGui(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.get_hosts()
-        self.set_table()
+        # self.get_hosts()
+        # self.set_table()
+        self.set_projects_tree()
+        self.groupStatus = QtWidgets.QButtonGroup()
+        self.groupStatus.addButton(self.radioButtonProjOn)
+        self.groupStatus.addButton(self.radioButtonProjOff)
+        self.groupStatus = QtWidgets.QButtonGroup()
         self.chooseFolderButton.clicked.connect(self.browse_folder)
         self.chooseWWWButton.clicked.connect(self.browse_www_folder)
-        self.createProjectButton.clicked.connect(self.create_host)
-        self.refreshHostsButton.clicked.connect(self.add_data)
+        self.saveProjectButton.clicked.connect(self.create_host)
+        self.treeServersProjects.itemClicked.connect(self.fill_fields)
+        self.radioExistFolder.toggled.connect(self.clear_fields)
+
+        # self.refreshHostsButton.clicked.connect(add_data)
+
+    def set_projects_tree(self):
+        self.treeServersProjects.clear()
+        apache = QtWidgets.QTreeWidgetItem(self.treeServersProjects, ['Apache'])
+        mysql = QtWidgets.QTreeWidgetItem(self.treeServersProjects, ['MySQL'])
+        self.treeServersProjects.addTopLevelItem(apache)
+        self.treeServersProjects.addTopLevelItem(mysql)
+        apache_status = get_server_info('apache2')
+        mysql_status = get_server_info('mysql')
+        apache_st = QtWidgets.QTreeWidgetItem(apache, [apache_status])
+        mysql_st = QtWidgets.QTreeWidgetItem(mysql, [mysql_status])
+        projects = get_projects_data()
+        for row in projects:
+            item = QtWidgets.QTreeWidgetItem(self.treeServersProjects, [row[1]])
+            self.treeServersProjects.addTopLevelItem(item)
+            QtWidgets.QTreeWidgetItem(item, ['host_name: ' + row[2]])
+            QtWidgets.QTreeWidgetItem(item, ['folder: ' + row[3]])
+            QtWidgets.QTreeWidgetItem(item, ['www: ' + row[4]])
+            QtWidgets.QTreeWidgetItem(item, ['a2_conf: ' + row[5]])
+            QtWidgets.QTreeWidgetItem(item, ['port: ' + str(row[6])])
+            QtWidgets.QTreeWidgetItem(item, ['status: ' + str(row[7])])
+
+    def fill_fields(self, it, col):
+        if it.text(col) == 'Apache' or it.text(col) == 'MySQL':
+            return
+        data = choose_project(it, col)
+        row = data[0]
+        self.projectNameLine.setText(row[1])
+        self.hostLine.setText(row[2])
+        self.lineIP.setText(get_host_ip(row[2]))
+        self.linePort.setText(str(row[6]))
+        self.projrctFolderLine.setText(row[3])
+        self.projrctWWWrLine.setText(row[4])
+        self.groupStatus.setExclusive(False)
+        self.radioButtonProjOn.setChecked(False)
+        self.radioButtonProjOff.setChecked(False)
+        self.groupStatus.setExclusive(True)
+        self.groupProjCreate.setExclusive(False)
+        self.radioExistFolder.setChecked(False)
+        self.groupProjCreate.setExclusive(True)
+        self.radioGit.setChecked(False)
+        self.groupFrame.setExclusive(False)
+        self.radioLuya.setChecked(False)
+        self.radioYii.setChecked(False)
+        self.radioWP.setChecked(False)
+        self.groupFrame.setExclusive(True)
+        self.repositoryLine.setText(get_git_url(row[3]))
+        if row[7] == 1:
+            self.radioButtonProjOn.setChecked(True)
+        if row[7] == 0:
+            self.radioButtonProjOff.setChecked(True)
+
+        if row[9] == 1:
+            self.radioLuya.setChecked(True)
+        if row[9] == 2:
+            self.radioYii.setChecked(True)
+        if row[9] == 3:
+            self.radioWP.setChecked(True)
+
+    def clear_fields(self):
+        self.projectNameLine.clear()
+        self.hostLine.clear()
+        self.lineIP.clear()
+        self.linePort.clear()
+        self.projrctFolderLine.clear()
+        self.projrctWWWrLine.clear()
+        self.radioButtonProjOn.setChecked(False)
+        self.radioButtonProjOff.setChecked(False)
+        self.radioLuya.setChecked(False)
+        self.radioYii.setChecked(False)
+        self.radioWP.setChecked(False)
 
     def browse_folder(self):
         self.projrctFolderLine.clear()
@@ -32,13 +168,6 @@ class appGUI(QtWidgets.QMainWindow, design.Ui_MainWindow):
         if directory:
             self.projrctWWWrLine.setText(directory)
 
-    def get_hosts(self):
-        self.confArea.clear()
-        hosts_folder = '/etc/apache2/sites-available/'
-        if hosts_folder:
-            for conf_name in os.listdir(hosts_folder):
-                self.confArea.addItem(conf_name)
-
     def project_type_choose(self):
         if self.radioExistFolder.isChecked():
             return 1
@@ -50,15 +179,23 @@ class appGUI(QtWidgets.QMainWindow, design.Ui_MainWindow):
         host_name = self.hostLine.text()
         project_folder = self.projrctFolderLine.text()
         www_folder = self.projrctWWWrLine.text()
-        if (not project_name or project_name is None or host_name == '' or host_name is None or project_folder == '' or project_folder is None):
-            QtWidgets.QMessageBox.about(self, "Error", "Not set:\nNo sudo pass\nProject name\nor\nHost name\nor\nProject folder")
+        if not project_name or project_name is None or host_name == '' or host_name is None or project_folder == '' or \
+                project_folder is None:
+            QtWidgets.QMessageBox.about(self, "Error", "Not set:\nNo sudo pass\nProject name\nor\nHost "
+                                                       "name\nor\nProject folder")
         else:
             if self.project_type_choose() == 1:
-                if (not www_folder or www_folder is None):
+                if not www_folder or www_folder is None:
                     QtWidgets.QMessageBox.about(self, "Error", "Not set:\nProject www folder")
                 else:
                     conf = open('/etc/apache2/sites-available/' + project_name + '.conf', 'w')
-                    conf.write('<VirtualHost *:80>\nServerName ' + host_name + '\nServerAdmin webmaster@localhost\nDocumentRoot ' + www_folder + '\n<Directory \"' + www_folder + '\">\nAllowOverride All\nRequire all granted\n</Directory>\nErrorLog ${APACHE_LOG_DIR}/error.log\nCustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>')
+                    conf.write('<VirtualHost *:80>\nServerName ' + host_name + '\nServerAdmin '
+                                                                               'webmaster@localhost\nDocumentRoot ' +
+                               www_folder + '\n<Directory \"' + www_folder + '\">\nAllowOverride All\nRequire all '
+                                                                             'granted\n</Directory>\nErrorLog ${'
+                                                                             'APACHE_LOG_DIR}/error.log\nCustomLog ${'
+                                                                             'APACHE_LOG_DIR}/access.log '
+                                                                             'combined\n</VirtualHost>')
                     conf.close()
                     os.system('a2ensite ' + project_name + '.conf')
                     os.system('systemctl reload apache2')
@@ -66,7 +203,9 @@ class appGUI(QtWidgets.QMainWindow, design.Ui_MainWindow):
                     hosts.write('127.0.0.1  ' + project_name + '.local\n')
                     hosts.close()
                     self.get_hosts()
+                    add_data(project_name, host_name, project_folder, www_folder, project_name + '.conf', 80, 1)
                     QtWidgets.QMessageBox.about(self, "Success", "Success")
+                    self.set_table()
             elif self.project_type_choose() == 2:
                 QtWidgets.QMessageBox.about(self, "Error", "Not set:\nProject www folder!!!!")
             else:
@@ -76,45 +215,14 @@ class appGUI(QtWidgets.QMainWindow, design.Ui_MainWindow):
         if os.geteuid() != 0:
             command = [sys.argv[0], sys.argv]
             os.execvp('sudo', ['sudo', 'python3'] + command)
-        sudopass, ok = QtWidgets.QInputDialog.getText(self, 'Set Sudo Password', 'Sudo password:')
+        surpass, ok = QtWidgets.QInputDialog.getText(self, 'Set Sudo Password', 'Sudo password:')
         if ok:
-            return sudopass
+            return surpass
 
-    def set_table(self):
-        table = self.get_projects_data()
-        rows = table.fetchall()
-        rows_num = len(rows)
-        print(rows_num)
-        self.tableProjects.setRowCount(rows_num)
-        self.tableProjects.setColumnCount(7)
-        self.tableProjects.setHorizontalHeaderLabels(["Project name", "Host name", "Project folder", "WWW Folder", "A2 config file", "Port", "Status"])
-        for row in rows:
-            inx = rows.index(row)
-            print(row[6])
-            self.tableProjects.insertRow(inx)
-            # add more if there is more columns in the database.
-            self.tableProjects.setItem(inx, 0, QtWidgets.QTableWidgetItem(row[1]))
-            self.tableProjects.setItem(inx, 1, QtWidgets.QTableWidgetItem(row[2]))
-            self.tableProjects.setItem(inx, 2, QtWidgets.QTableWidgetItem(row[3]))
-            self.tableProjects.setItem(inx, 3, QtWidgets.QTableWidgetItem(row[4]))
-            self.tableProjects.setItem(inx, 4, QtWidgets.QTableWidgetItem(row[5]))
-            self.tableProjects.setItem(inx, 5, QtWidgets.QTableWidgetItem(str(row[6])))
-            self.tableProjects.setItem(inx, 6, QtWidgets.QTableWidgetItem(str(row[7])))
-
-    def get_projects_data(self):
-        return controllers.sqlite_controller.getDatabase()
-
-    def add_data(self):
-        controllers.sqlite_controller.addData()
-        # conn = sqlite3.connect('databases/projects')
-        # cursor = conn.cursor()
-        # add = cursor.execute("""INSERT INTO projects (name, host_name, folder, www_folder, port, status)
-        #           VALUES('test','test.local','folder','www', 80, 1) """)
-        # print(add)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = appGUI()
+    window = AppGui()
     window.show()
     app.exec_()
 
